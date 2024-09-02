6686b1b868bada9c18546efa9ffb2fe8cfa33ed6
durve-greycube <zelam@greycube.in>
2024-09-02 14:57:06 +0530

    fix:project json clean up, create FRI,PP,PPC,PC from project

--- rowad_project/rowad_project/doctype/price_determination_form_rm/price_determination_form_rm.py
+++ rowad_project/rowad_project/doctype/price_determination_form_rm/price_determination_form_rm.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2024, GreyCube Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
+from frappe.utils import get_link_to_form
 from frappe.model.document import Document
 
 
 class PriceDeterminationFormRM(Document):
-	pass
+	def validate(self):
+		if self.competition_status == "Won":
+			project_doc = frappe.new_doc("Project")
+			project_doc.project_name = self.rm_project_name
+			project_doc.save(ignore_permissions = True)
+			frappe.msgprint(_("Project is created {0}".format(get_link_to_form("Project",project_doc.name))),alert=True)
